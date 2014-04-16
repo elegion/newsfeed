f8a7cd084f11fb9a9b90d1ef5116eb3f9b80393a
Daniel Serdyukov <dvp.troy@gmail.com>
2014-04-16 21:17:51 +0400

    init project

---
+++ src/main/java/com/elegion/newsfeed/account/Authenticator.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2012-2014 Daniel Serdyukov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.elegion.newsfeed.account;
+
+import android.accounts.AbstractAccountAuthenticator;
+import android.accounts.Account;
+import android.accounts.AccountAuthenticatorResponse;
+import android.accounts.NetworkErrorException;
+import android.content.Context;
+import android.os.Bundle;
+
+/**
+ * @author Daniel Serdyukov
+ */
+public class Authenticator extends AbstractAccountAuthenticator {
+
+    public Authenticator(Context context) {
+        super(context);
+    }
+
+    @Override
+    public Bundle editProperties(AccountAuthenticatorResponse response, String accountType) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Bundle addAccount(AccountAuthenticatorResponse response, String accountType, String authTokenType,
+                             String[] requiredFeatures, Bundle options)
+            throws NetworkErrorException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Bundle confirmCredentials(AccountAuthenticatorResponse response, Account account, Bundle options)
+            throws NetworkErrorException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Bundle getAuthToken(AccountAuthenticatorResponse response, Account account, String authTokenType,
+                               Bundle options) throws NetworkErrorException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String getAuthTokenLabel(String authTokenType) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Bundle updateCredentials(AccountAuthenticatorResponse response, Account account, String authTokenType,
+                                    Bundle options) throws NetworkErrorException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Bundle hasFeatures(AccountAuthenticatorResponse response, Account account, String[] features)
+            throws NetworkErrorException {
+        throw new UnsupportedOperationException();
+    }
+
+}
---
+++ src/main/java/com/elegion/newsfeed/activity/MainActivity.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2012-2014 Daniel Serdyukov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.elegion.newsfeed.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.elegion.newsfeed.R;
+
+/**
+ * @author Daniel Serdyukov
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ac_single_frame);
+    }
+
+}
